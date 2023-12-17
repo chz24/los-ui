89b972098de817cfc1e77c6663c579d63372e6b5
chz24 <fadhli.ubai@gmail.com>
2023-12-17 17:35:10 +0700

    routing stuff

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import Navbar from "@/components/Navbar.vue";
 </script>
 
 <template>
   <div class="pt-16">
-    <Navbar />
-    <RouterView />
+    <navbar v-if="$route.meta.showNavbar"/>
+    <router-view />
   </div>
 </template>
 
