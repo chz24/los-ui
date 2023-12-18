f24c9756bc61406516730f5ae12c474d064d218e
Steven Kristian <steven.kristian@gdn-commerce.com>
2023-12-18 19:13:33 +0700

    add design for dashboard view

---
+++ src/components/PopupCreate.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <div class="container w-2/12 h-1/3 border border-[#0098c0]">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+</style>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -2,6 +2,8 @@
 import { Qalendar } from "qalendar";
 
 import { useDashboardStore } from "@/stores/dashboard.js";
+import PopupCreate from "@/components/PopupCreate.vue";
+import PopupEdit from "@/components/PopupEdit.vue";
 
 const dashboardStore = useDashboardStore()
 
@@ -25,7 +27,11 @@ const events = [
 const configs = {
   locale: "en-US",
   defaultMode: "month",
+  week: {
+    startsOn: "sunday"
+  },
   style: {
+    fontFamily: "Noto Sans",
     colorSchemes: {
       meetings: {
         color: "#fff",
@@ -41,27 +47,27 @@ const configs = {
 </script>
 
 <template class="container">
-  <div>
-    <label class="relative inline-flex items-center cursor-pointer">
-      <input type="checkbox" value="" class="sr-only peer">
-      <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-      <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Toggle me</span>
-    </label>
-  </div>
-  <div class="flex container mx-auto">
-    <Qalendar
-        class="p-4"
-        :selected-date="new Date()"
-        :events="events"
-        :config="configs"
-        @date-was-clicked="showDateMenu"
-    />
-    <div class="w-1/5 p-4" v-if="dashboardStore.getIsDateMenuShown">
-      <p>haloo</p>
-      <fwb-toggle v-model="hehe" label="Toggle me" />
+  <div class="container max-w-2xl max-y-4xl mx-auto my-40 is-light-mode">
+    <div class="flex mb-4 align">
+      <p class="text-xl">Reservation Calendar</p>
+      <div class="my-auto ml-auto">
+        <label class="relative inline-flex items-center cursor-pointer">
+          <input type="checkbox" value="" class="sr-only peer">
+          <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+          <span class="ms-3 text-sm">Recurring</span>
+        </label>
+      </div>
     </div>
+      <Qalendar
+          class="w-full"
+          :selected-date="new Date()"
+          :events="events"
+          :config="configs"
+          @date-was-clicked="showDateMenu"
+      />
   </div>
-
+  <popup-create />
+  <popup-edit />
 
 </template>
 
