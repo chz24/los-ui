d45b873b5970eefabe1fe70bf30dd5a69238d094
Muhammad Fadhli Ubaidilah <muhammad.ubaidilah@gdn-commerce.com>
2023-12-19 12:45:17 +0700

    local storage reservation

--- src/stores/reservation.js
+++ src/stores/reservation.js
@@ -3,18 +3,18 @@ import { useStorageStore } from "@/stores/storage.js";
 
 const STORE_NAME = 'reservation'
 
-const getDefaultSettings = () => ([
+const getDefaultSettings= [
     {
-    userId: "1291820a-1ecd-4a62-afbb-5986504155ae",
-    date: "2023-12-12",
-    orderLunch: true
+        userId: "1291820a-1ecd-4a62-afbb-5986504155ae",
+        date: "2023-12-12",
+        orderLunch: true
     }
-  ])
+]
 
 const getReservations = () => {
     const reservations = localStorage.getItem(STORE_NAME)
 
-    return reservations ? JSON.parse(reservations) : getDefaultSettings
+    return reservations ? JSON.parse(reservations).reservations : getDefaultSettings
 }
 
 export const useReservationStore = defineStore(STORE_NAME, {
@@ -29,17 +29,15 @@ export const useReservationStore = defineStore(STORE_NAME, {
         getCurrentUserReservations() {
             const storageStore = useStorageStore()
             const {settings} = storeToRefs(storageStore)
-            if (this.reservations !== null) {
-                return this.reservations.filter(reservation => reservation.userId === settings.value.id)
-            }
-            return null
+
+            return this.reservations.filter(reservation => reservation.userId === settings.value.id)
         }
     },
 
     actions: {
         addReservations(reservation) {
             this.reservations.push(reservation)
-            localStorage.setItem(STORE_NAME, JSON.stringify(this.reservations))
+            localStorage.setItem(STORE_NAME, JSON.stringify({reservations : this.reservations}))
         },
 
         editReservation(userId, reservation) {
